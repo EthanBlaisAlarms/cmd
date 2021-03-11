f752fb0aeb0554b80f052c1c82d85529c1250285
EBA <ethanblaisalarms@gmail.com>
2021-03-11 12:09:28 -0500

    Update contactus.php

--- php/contactus.php
+++ php/contactus.php
@@ -3,7 +3,7 @@
     <?php
     $time = date("h:i:sa m/d/Y")
     $subject = $_POST["subject"];
-    $msg ="From: $_POST["email"]\nTime: $time\nMessage: $_POST["description"]";
+    $msg ="From: $_POST["email"] ($_POST["name"])\nTime: $time\nMessage: $_POST["description"]";
     mail("ethanblaisalarms@gmail.com","Contact You - $subject",$msg);
     echo "Submitted!";
   </body>
