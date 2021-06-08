31a3e80ebdfbf32bc14b2ecaec87ba8f4eb9cacf
EBA <official-eba@outlook.com>
2021-06-08 19:55:44 -0400

    Create countdown.js

---
+++ countdown.js
@@ -0,0 +1,19 @@
+function countdown() {
+  var i = document.getElementById('counter');
+  if (parseInt(i.innerHTML)<=0) {
+    location.href = 'index.html';
+  }
+  if (parseInt(i.innerHTML)!=0) {
+    i.innerHTML = parseInt(i.innerHTML)-1;
+  }
+  countdownSec();
+}
+function countdownSec() {
+  var x = document.getElementById('sec');
+  if (parseInt(i.innerHTML)=1) {
+    x.innerHTML = 'second';
+  } else {
+    x.innerHTML = 'seconds';
+  }
+}
+setInterval(function(){ countdown(); },1000);
