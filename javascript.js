fa74926a959a2482037fa8c2507c50ad95a17f0c
EBA <official-eba@outlook.com>
2021-06-08 19:48:36 -0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+$(function(){
+  $("#header").load("header.html");
+  $("#footer").load("footer.html");
+});
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "69wj77p43q");
+function download(fileDir, name) {
+  var a = document.createElement("a");
+  a.href = fileDir;
+  a.setAttribute("download", name);
+  a.click();
+}
