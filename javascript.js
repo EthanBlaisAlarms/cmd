c9032d2fffbe5ee8c89563163179ffd6292d2751
EBA <official-eba@outlook.com>
2021-11-10 09:36:38 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,14 +2,3 @@ $(function(){
   $("#header").load("https://ethanblaisalarms.github.io/header.html");
   $("#footer").load("https://ethanblaisalarms.github.io/footer.html");
 });
-(function(c,l,a,r,i,t,y){
-  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "69wj77p43q");
-function download(fileDir, name) {
-  var a = document.createElement("a");
-  a.href = fileDir;
-  a.setAttribute("download", name);
-  a.click();
-}
